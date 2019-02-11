3796ee3372abfa1d955c155b28a1522a7d1fbf20
Anas BEN EL MAHJOUB <anas.ben-el-mahjoub@dauphine.eu>
2019-02-11 09:30:01 +0100

    student preference updated

--- src/main/java/io/github/oliviercailloux/y2018/StudentsPreference.java
+++ src/main/java/io/github/oliviercailloux/y2018/StudentsPreference.java
@@ -4,12 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import com.google.common.base.Strings;
+
 public class StudentsPreference {
 
 	private Map<Person, Set<RawPreference>> studPref;
+	
+	private static Jsonb jsonb = JsonbBuilder.create();
 
 	public StudentsPreference() {
 		studPref = new HashMap<>();
+	}
+	
+	public StudentsPreference(Person student, Set<RawPreference> les_preferences) {
+		this.studPref = new HashMap<>();
+		this.studPref.put(student, les_preferences);
+				
 	}
 
 	public Map<Person, Set<RawPreference>> getStudPref() {
@@ -22,6 +35,14 @@ public class StudentsPreference {
 
 	public Set<RawPreference> getPreference(Person student) {
 		return studPref.get(student);
+	}
+	
+	public String studentsPreferenceToJson() {
+		return jsonb.toJson(this);
+	}
+	
+	public static StudentsPreference jsonToStudentsPreference(String jsonPreference) {
+		return jsonb.fromJson(Strings.nullToEmpty(jsonPreference), StudentsPreference.class);
 	}
 
 }
