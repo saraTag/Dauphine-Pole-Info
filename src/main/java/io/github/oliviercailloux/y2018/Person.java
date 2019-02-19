56b9f0d720e017d2eee2206c3bd7971fa59c2d58
zakaria jannani <zakaria jannani>
2019-02-19 19:13:51 +0100

    update person

--- src/main/java/io/github/oliviercailloux/y2018/Person.java
+++ src/main/java/io/github/oliviercailloux/y2018/Person.java
@@ -3,56 +3,63 @@ package io.github.oliviercailloux.y2018;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.json.bind.annotation.JsonbPropertyOrder;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import com.google.common.base.Strings;
 
 @Entity
-@JsonbPropertyOrder({ "id", "firstname", "lastname" })
-@XmlRootElement
+@Table(name = "personne")
 public class Person {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@XmlAttribute
 	private int id;
-
+	
+	@Column(name = "prenom", unique = false)
 	private String firstname;
 
+	@Column(name = "nom", unique = false)
 	private String lastname;
 
 	private String email;
 
+	@Column(name = "telephone", unique = false)
 	private String phone;
 
 	private String fax;
 
+	@Column(name = "home_page", unique = false)
 	private String homePage;
 
 	private String cv;
-
+	
+	@Column(name = "notes", unique = false)
 	private String note; // comment renommage !!!
 
 	private String password;
 
+	@Column(name = "roles", unique = false)
 	private String role;
+
+	@Column(name = "adresse", unique = false)
+	private String address;
+
+	private String mobile;
+
+	@Column(name = "vacataire", unique = false)
+	private String temporary;
 
 	@OneToOne
 	private Master master;
 
 	private int yearMaster;
-
-	private String address;
-
-	private String mobile;
-
-	private String temporary;
 
 	private static Jsonb jsonb = JsonbBuilder.create();
 
@@ -83,7 +90,7 @@ public class Person {
 		return id;
 	}
 
-	public void setIde(int idPerson) {
+	public void setId(int idPerson) {
 		this.id = idPerson;
 	}
 
