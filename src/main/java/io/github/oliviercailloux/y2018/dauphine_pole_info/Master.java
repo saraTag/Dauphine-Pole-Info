b688900734f4bdefff2156ef0d820ce7f8318ae6
Anas BEN EL MAHJOUB <anas.ben-el-mahjoub@dauphine.eu>
2019-03-04 19:23:43 +0100

    renaming packages

---
+++ src/main/java/io/github/oliviercailloux/y2018/dauphine_pole_info/Master.java
@@ -0,0 +1,96 @@
+package io.github.oliviercailloux.y2018.dauphine_pole_info;
+
+import java.util.Optional;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.annotation.JsonbProperty;
+import javax.json.bind.annotation.JsonbPropertyOrder;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.common.base.Strings;
+
+@Entity
+@JsonbPropertyOrder({ "id", "name", "description" })
+@XmlRootElement
+public class Master {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@XmlAttribute @JsonbProperty("id")
+	private int id;
+	
+	@JsonbProperty("name")
+	private String name;
+
+	@JsonbProperty("description")
+	private Optional<String> description;
+
+	private static Jsonb jsonb = JsonUtils.getInstance();
+
+	public Master() {
+		super();
+		this.name = "";
+
+	}
+
+	public Master(int id, String nom, String description) {
+		super();
+		this.id = id;
+		this.name = Strings.nullToEmpty(nom);
+		this.description = Optional.of(description);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return not null.
+	 */
+	@XmlAttribute(name = "name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = Strings.nullToEmpty(name);
+	}
+
+	/**
+	 * @return not null.
+	 */
+	@XmlAttribute(name = "description")
+	public Optional<String> getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = Optional.of(description);
+	}
+
+	/**
+	 * @return Master : json
+	 * 
+	 */
+	public String toJson() {
+		return jsonb.toJson(this);
+	}
+
+	/**
+	 * @param jsonMaster
+	 *            : String
+	 * @return Object : Master not null
+	 */
+	public static Master fromJson(String jsonbMaster) {
+		return jsonb.fromJson(Strings.nullToEmpty(jsonbMaster), Master.class);
+	}
+}
