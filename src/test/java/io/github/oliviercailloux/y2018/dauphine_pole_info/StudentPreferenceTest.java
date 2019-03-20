f3f3fd14acd4b692004904ba41b3c6ad90b744cb
zakaria jannani <zakaria jannani>
2019-03-20 22:55:35 +0100

    update StudentPreferenceTest

--- src/test/java/io/github/oliviercailloux/y2018/dauphine_pole_info/StudentPreferenceTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/dauphine_pole_info/StudentPreferenceTest.java
@@ -93,7 +93,7 @@ public class StudentPreferenceTest {
 
 	@Test
 	public void JsonToStudentPreference() {
-		String jsonResult = "{\"studentPreference\":[{\"person\":\"{\\\"id\\\":1,\\\"firstname\\\":\\\"firstname\\\",\\\"lastname\\\":\\\"lastname\\\",\\\"year_master\\\":0}\"},[{\"preference\":\"{\\\"master\\\":{\\\"id\\\":1,\\\"name\\\":\\\"SITN\\\",\\\"description\\\":\\\"Description\\\"},\\\"level\\\":100}\"}]]}";
+		String jsonResult = "{"studentPreference":[{"person":"\n{\n    \"id\": 1,\n    \"firstname\": \"firstname\",\n    \"lastname\": \"lastname\",\n    \"year_master\": 0\n}"},[{"preference":"{\"master\":{\"id\":1,\"name\":\"SITN\",\"description\":\"Description\"},\"level\":100}"}]]};
 		StudentPreference testDecodeTest = StudentPreference.jsonToStudentPreference(jsonResult);
 		assertNotNull(testDecodeTest);
 		
